b1fbd7fe0aa0e63e44a3131ac8d7a1ee3d096c58
Joey Orlando <joseph.t.orlando@gmail.com>
2017-03-27 12:04:26 -0400

    comment out hardware stuff for now

--- web_interface/api/controllers/__init__.py
+++ web_interface/api/controllers/__init__.py
@@ -1,8 +1,8 @@
 from api import api
 from flask import request, render_template
-from hardware.car import Car
-
-car = Car()
+# from hardware.car import Car
+#
+# car = Car()
 
 
 @api.route("/", methods=["GET"])
@@ -15,16 +15,16 @@ def drive_the_car():
 	request_body = request.get_json()
 	command = request_body.get("command")
 
-	if '37' in command:
-		car.forward_left(100)
-	elif '38' in command:
-		car.forward(100)
-	elif '39' in command:
-		car.forward_right(100)
-	elif '40' in command:
-		car.backward(100)
-	else:
-		car.stop()
+	# if '37' in command:
+	# 	car.forward_left(100)
+	# elif '38' in command:
+	# 	car.forward(100)
+	# elif '39' in command:
+	# 	car.forward_right(100)
+	# elif '40' in command:
+	# 	car.backward(100)
+	# else:
+	# 	car.stop()
 
 
 @api.route("/store_logs", methods=["GET"])
