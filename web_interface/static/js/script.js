02cb7e0df841c868731c46517afe9dfe8f4d23e6
Joey Orlando <joseph.t.orlando@gmail.com>
2019-04-06 15:59:44 -0400

    modify frontend

--- web_interface/static/js/script.js
+++ web_interface/static/js/script.js
@@ -1,35 +1,31 @@
 var keys = {};
 
-$(document).keydown(function (e) {
-	keys[e.which] = true;
+$(document).keydown(function(e) {
+  keys[e.which] = true;
 
-	console.log(keys);
+  console.log(keys);
 
-	var json_upload = JSON.stringify({
-		command: keys
-	});
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.open('POST', '/drive');
-	xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xmlhttp.send(json_upload);
+  var json_upload = JSON.stringify({command: keys});
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open('POST', 'http://192.168.2.171:5001/drive');
+  xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xmlhttp.send(json_upload);
 
-	// printKeys();
+  // printKeys();
 });
 
-$(document).keyup(function (e) {
-	delete keys[e.which];
+$(document).keyup(function(e) {
+  delete keys[e.which];
 
-	console.log(keys);
+  console.log(keys);
 
-	var json_upload = JSON.stringify({
-		command: keys
-	});
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.open('POST', '/drive');
-	xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xmlhttp.send(json_upload);
+  var json_upload = JSON.stringify({command: keys});
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open('POST', 'http://192.168.2.171:5001/drive');
+  xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xmlhttp.send(json_upload);
 
-	// printKeys();
+  // printKeys();
 });
 
 // function printKeys() {
