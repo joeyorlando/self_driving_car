270a32bde1c061942e835046dba8f1e92834b3d6
Joey Orlando <joseph.t.orlando@gmail.com>
2019-04-06 17:15:45 -0400

    change gunicorn config

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -2,6 +2,6 @@ bind = "0.0.0.0:5001"
 workers=2
 preload = True
 accesslog = "-"
-loglevel = "DEBUG"
+loglevel = "debug"
 proc_name = "web_api"
 access_log_format = "%(h)s %(l)s %(u)s %(t)s .%(r)s. %(s)s %(b)s .%(f)s. .%(a)s. host=\"%({Host}i)s\""
