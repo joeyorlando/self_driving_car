5924e3800b46ad18362bac24637d08479e5afe5b
Joey Orlando <joseph.t.orlando@gmail.com>
2019-04-06 16:43:17 -0400

    modify frontend

--- web_interface/static/js/script.js
+++ web_interface/static/js/script.js
@@ -1,25 +1,27 @@
 var keys = {};
 
 const makeDriveCall = () => {
-  fetch('http://192.168.2.171:5001/drive', {
-    method: 'POST',
-    headers: new Headers({
-      'Content-Type': 'application/json',
-    }),
-    body: {command: keys}
-  });
+	fetch('http://192.168.2.171:5001/drive', {
+		method: 'POST',
+		headers: new Headers({
+			'Content-Type': 'application/json',
+		}),
+		body: JSON.stringify({
+			command: keys
+		})
+	});
 };
 
 $(document).keydown(e => {
-  keys[e.which] = true;
-  makeDriveCall();
+	keys[e.which] = true;
+	makeDriveCall();
 });
 
 $(document).keyup(e => {
-  delete keys[e.which];
-  makeDriveCall();
+	delete keys[e.which];
+	makeDriveCall();
 });
 
 $('#record').click(e => {
-  $(this).attr('Value', 'Stop Recording');
+	$(this).attr('Value', 'Stop Recording');
 });
