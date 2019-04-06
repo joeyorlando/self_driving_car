19e816baca1a7f810636efea572aac3f8b44e3a2
Joey Orlando <joseph.t.orlando@gmail.com>
2019-04-06 16:52:30 -0400

    modify frontend

--- web_interface/static/js/script.js
+++ web_interface/static/js/script.js
@@ -1,39 +1,39 @@
 var keys = {};
 
 const makeDriveCall = () => {
-	fetch('http://192.168.2.171:5001/drive', {
-		method: 'POST',
-		headers: new Headers({
-			'Content-Type': 'application/json',
-		}),
-		body: JSON.stringify({
-			command: keys
-		})
-	});
+  fetch('http://192.168.2.171:5001/drive', {
+    method: 'POST',
+    headers: new Headers({
+      'Content-Type': 'application/json',
+    }),
+    body: JSON.stringify({command: keys})
+  });
 };
 
-$(document).keydown(e => {
-	keys[e.which] = true;
-	makeDriveCall();
-});
+$(document).ready(() => {
+  $(document).keydown(e => {
+    keys[e.which] = true;
+    makeDriveCall();
+  });
 
-$(document).keyup(e => {
-	delete keys[e.which];
-	makeDriveCall();
-});
+  $(document).keyup(e => {
+    delete keys[e.which];
+    makeDriveCall();
+  });
 
-$('#record').click(function () {
-	const isRecording = $(this).val() === 'Stop Recording';
-	let newVal;
+  $('#record').click(function() {
+    const isRecording = $(this).val() === 'Stop Recording';
+    let newVal;
 
-	console.log('YOOOO', $(this).val());
+    console.log('YOOOO', $(this).val());
 
-	if (isRecording) {
-		// Make API call here...
-		newVal = 'Start Recording';
-	} else {
-		// Make API call here...
-		newVal = 'Stop Recording';
-	}
-	$(this).attr('value', newVal);
+    if (isRecording) {
+      // Make API call here...
+      newVal = 'Start Recording';
+    } else {
+      // Make API call here...
+      newVal = 'Stop Recording';
+    }
+    $(this).attr('value', newVal);
+  });
 });
