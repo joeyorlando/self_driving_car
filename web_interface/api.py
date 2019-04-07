444c5e4eb6890889bbe6430267b5fc7ddad19608
Joey Orlando <joseph.t.orlando@gmail.com>
2019-04-07 10:47:48 -0400

    comment out car.stop() in api controller

--- web_interface/api.py
+++ web_interface/api.py
@@ -3,10 +3,13 @@ import time
 from flask import Flask, request, render_template, Response, jsonify
 from hardware.car import Car
 
-template_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), './templates'))
-static_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), './static'))
+template_folder = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), './templates'))
+static_folder = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), './static'))
 
-api = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+api = Flask(__name__, template_folder=template_folder,
+            static_folder=static_folder)
 car = Car()
 recording = False
 output_data_folder_name = "./data/training/%s" % int(time.time())
@@ -17,41 +20,45 @@ print("FOLDER NAME IS %s" % output_data_folder_name)
 
 @api.route("/", methods=["GET"])
 def get_webpage():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @api.route("/video_feed", methods=["GET"])
 def get_video_stream():
-	"""
-		https://blog.miguelgrinberg.com/post/video-streaming-with-flask
-	"""
-	return Response(car.camera.stream(output_data_folder_name), mimetype="multipart/x-mixed-replace; boundary=frame")
+    """
+            https://blog.miguelgrinberg.com/post/video-streaming-with-flask
+    """
+    return Response(car.camera.stream(output_data_folder_name), mimetype="multipart/x-mixed-replace; boundary=frame")
+
 
 @api.route("/drive", methods=["POST"])
 def drive_the_car():
-	request_body = request.get_json()
-	command = request_body.get("command")
+    request_body = request.get_json()
+    command = request_body.get("command")
 
-	if '37' in command:
-		car.turn_left()
-	elif '38' in command:
-		car.drive_forward()
-	elif '39' in command:
-		car.turn_right()
-	elif '40' in command:
-		car.drive_backward()
-	else:
-		car.stop()
+    if '37' in command:
+        car.turn_left()
+    elif '38' in command:
+        car.drive_forward()
+    elif '39' in command:
+        car.turn_right()
+    elif '40' in command:
+        car.drive_backward()
+    # else:
+    # 	car.stop()
 
-	return 'Ok'
+    return 'Ok'
+
 
 @api.route("/start_recording", methods=["POST"])
 def start_recording():
-	global recording
-	recording = True
-	return 'Ok'
+    global recording
+    recording = True
+    return 'Ok'
+
 
 @api.route("/stop_recording", methods=["POST"])
 def stop_recording():
-	global recording
-	recording = False
-	return 'Ok'
+    global recording
+    recording = False
+    return 'Ok'
